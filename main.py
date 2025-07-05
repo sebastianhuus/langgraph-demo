5144bbfee8d5f56f8b9026c5b562af4b72c40da8
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-07-05 18:08:52 +0200

    reafactor(main): condense graph builder code together

--- main.py
+++ main.py
@@ -20,10 +20,8 @@ def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
 
 graph_builder.add_node("chatbot", chatbot)
-
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
-
 graph = graph_builder.compile()
 
 user_input = input("Enter a message: ")
@@ -31,4 +29,4 @@ messages = [{"role": "system", "content": "You are a helpful assistant called Ti
 state = graph.invoke({"messages": messages + [{"role": "user", "content": user_input}]})
 response = state["messages"][-1]
 print(response.content)
-print(state)
+print(state) 
